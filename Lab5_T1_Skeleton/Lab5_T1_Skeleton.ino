5fb2ba3af6ca4dae21666d7f3a6a6203226dc415
Kenyu <kenyu317@gmail.com>
2025-11-17 15:50:17 +0800

    done

--- Lab5_T1_Skeleton/Lab5_T1_Skeleton.ino
+++ Lab5_T1_Skeleton/Lab5_T1_Skeleton.ino
@@ -1,10 +1,10 @@
 /*Change all the ? in the code and add code in ??? to Control the speed of rotation.*/
 
-#define IN1   ?  // Replace the ? with the GPIO pin you selected to connect IN1
-#define IN2   ?   // Replace the ? with the GPIO pin you selected to connect IN2
-#define A  ? // Replace the ? with the GPIO pin you selected to connect encoder A
-#define B  ? // Replace the ? with the GPIO pin you selected to connect encoder B
-#define PWM ?  // Replace the ? with the GPIO pin you selected to output PWM
+#define IN1   26  // Replace the ? with the GPIO pin you selected to connect IN1
+#define IN2   25   // Replace the ? with the GPIO pin you selected to connect IN2
+#define A  34 // Replace the ? with the GPIO pin you selected to connect encoder A
+#define B  35 // Replace the ? with the GPIO pin you selected to connect encoder B
+#define PWM 14  // Replace the ? with the GPIO pin you selected to output PWM
 
 int A_data=0;
 int B_data=0;
@@ -13,41 +13,54 @@ String command;
 
 void setup() {
   /*setup baud and pin mode */
-  ??? 
+  Serial.begin(115200);
+  pinMode(26,OUTPUT);
+  pinMode(25,OUTPUT);
+  pinMode(34,INPUT);
+  pinMode(35,INPUT);
+  pinMode(14,OUTPUT);
 
   /*Set a rotation direction*/
-  ???
+  
 }
 
 void loop() {
-   
+
+    digitalWrite(IN1, 0);   // cloclwise
+    digitalWrite(IN2, 1);
     if (Serial.available() > 0) {
         command = Serial.readStringUntil('\n'); // Read the incoming command
         command.trim(); // Remove any leading or trailing whitespace
         if (command == "0") {
         // output PWM signals with 0% duty cycle
-        analogWrite(PWM,?);  
+        analogWrite(PWM,0);  
         } 
         else if (command == "25") {
         // output PWM signals with 25% duty cycle
-        analogWrite(PWM,?);   
+        analogWrite(PWM,63.75);   
         }
         else if (command == "50") {
         // output PWM signals with 50% duty cycle
-        analogWrite(PWM,?);   
+        analogWrite(PWM,127.5);   
         }
         else if (command == "75") {
         // output PWM signals with 75% duty cycle
-        analogWrite(PWM,?);   
+        analogWrite(PWM,191.25);   
         }
         else if (command == "100") {
         // output PWM signals with 100% duty cycle
-        analogWrite(PWM,?);   
+        analogWrite(PWM,255);   
         }
         }
     // Read values of A and B
-       ???
+        A_data=digitalRead(A);
+        B_data=digitalRead(B);
     // Plot A B in Serial Plotter
-       ??? 
+        Serial.print("A:");
+        Serial.print(A_data);
+        Serial.print(",");
+        Serial.print("B:");
+        Serial.print(B_data);
+        Serial.println("\t"); 
     
 }
--- Lab5_T2_Skeleton/Lab5_T2_Skeleton.ino
+++ Lab5_T2_Skeleton/Lab5_T2_Skeleton.ino
@@ -1,14 +1,14 @@
 /*Change all the ? in the code and add code in ??? to Control 2 DC Motors.*/
 
-#define IN1   ?  // Replace the ? with the GPIO pin you selected to connect IN1
-#define IN2   ?   // Replace the ? with the GPIO pin you selected to connect IN2
-#define AL  ? // Replace the ? with the GPIO pin you selected to connect encoder A of Motor L
-#define BL  ? // Replace the ? with the GPIO pin you selected to connect encoder B of Motor L
+#define IN1   27  // Replace the ? with the GPIO pin you selected to connect IN1
+#define IN2   32   // Replace the ? with the GPIO pin you selected to connect IN2
+#define AL  34 // Replace the ? with the GPIO pin you selected to connect encoder A of Motor L
+#define BL  35// Replace the ? with the GPIO pin you selected to connect encoder B of Motor L
 
-#define IN3   ?  // Replace the ? with the GPIO pin you selected to connect IN3
-#define IN4   ?   // Replace the ? with the GPIO pin you selected to connect IN4
-#define AR  ? // Replace the ? with the GPIO pin you selected to connect encoder A of Motor R
-#define BR ? // Replace the ? with the GPIO pin you selected to connect encoder B of Motor R
+#define IN3  16   // Replace the ? with the GPIO pin you selected to connect IN3
+#define IN4   17   // Replace the ? with the GPIO pin you selected to connect IN4
+#define AR  36// Replace the ? with the GPIO pin you selected to connect encoder A of Motor R
+#define BR 13// Replace the ? with the GPIO pin you selected to connect encoder B of Motor R
 
 
 int AL_data=0;
@@ -21,7 +21,18 @@ String command;
 
 void setup() {
   /*setup baud and pin mode */
-   ??? 
+   Serial.begin(115200);
+   pinMode(IN1,OUTPUT);
+   pinMode(IN2,OUTPUT);
+   pinMode(AL,INPUT);
+   pinMode(BL,INPUT);
+   pinMode(AR,INPUT);
+   pinMode(BR,INPUT);
+   pinMode(IN3,OUTPUT);
+   pinMode(IN4,OUTPUT);
+
+
+
 
 
 }
@@ -34,28 +45,59 @@ void loop() {
     if (command == "F") 
     { 
       // Move forward 
-       ??? 
+      digitalWrite(IN1,HIGH);
+      digitalWrite(IN2,LOW);
+      digitalWrite(IN3,HIGH);
+      digitalWrite(IN4,LOW);
       } 
     else if (command == "B") 
     { // Move Backward 
-      ??? 
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,HIGH);
+      digitalWrite(IN3,LOW);
+      digitalWrite(IN4,HIGH);
       } 
     else if (command == "R") 
     { // Turn right 
-   ???
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW); 
+    digitalWrite(IN4,LOW);
     } 
     else if (command == "L") { 
     // Turn left 
-    ???
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,HIGH);
+    digitalWrite(IN4,LOW);
    } 
   else if (command == "S") { 
     // Stop 
-    ???
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
      } 
         }
     // Read values of A and B of both Motor L and R
-     ???
+     AL_data=digitalRead(AL);
+     BL_data=digitalRead(BL);
+     AR_data=digitalRead(AR);
+     BR_data=digitalRead(BR);
     // Plot A and B of both Motor L and R in Serial Plotter
-    ???
+        Serial.print("AL:");
+        Serial.print(AL_data);
+        Serial.print(",");
+        Serial.print("BL:");
+        Serial.print(BL_data);
+        Serial.println("\t"); 
+
+        Serial.print("AR:");
+        Serial.print(AR_data);
+        Serial.print(",");
+        Serial.print("BR:");
+        Serial.print(BR_data);
+        Serial.println("\t"); 
+
     
 }
